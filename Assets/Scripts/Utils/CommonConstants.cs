35ff45efaec41fb50c68cfcfff02a17fb3cb5ac3
thongtruong <thong1411998@gmail.com>
2022-10-10 19:40:17 +0700

    - First setup board

---
+++ Assets/Scripts/Utils/CommonConstants.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonConstans
+{
+    public class CommonConstants
+    {
+
+    }
+
+    public class StringConstants
+    {
+        
+    }
+}
+
+
