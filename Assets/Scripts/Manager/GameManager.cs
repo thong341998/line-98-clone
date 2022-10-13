4fe41432acf5532a30a0e09a868e2f80af36d049
thongtruong <thong1411998@gmail.com>
2022-10-14 01:05:24 +0700

    - Build android for release

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -13,18 +13,39 @@ public enum GameState
     GameWin
 }
 
+public enum ScreenRatio
+{
+    SuperNarrow,
+    VeryNarrow,
+    Narrow,
+    Normal,
+    Broad,
+    VeryBroad,
+}
+
 public class GameManager : MonoBehaviour
 {
     public static event System.Action<GameState,GameState> OnGameStateChanged = delegate { };
 
     public static GameManager Instance = null;
+    [SerializeField] private Camera gameplayCamera;
+    [SerializeField] private ScreenRatio _screenRatio;
+    public ScreenRatio ScreenRatio
+    {
+        get { return _screenRatio; }
+        private set { _screenRatio = value; }
+    }
 
-   
     [SerializeField]
     private GameState _gameState = GameState.Prepare;
     [SerializeField]
     private int targetFrameRate = 60;
     public int scoreAmountPerBall = 10;
+
+    private float responsiveFactor;
+    private static readonly float RESPONSIVE_SIZE = 5;
+    private static readonly Vector2 RESPONSIVE_SCREEN = new Vector2(480, 800);
+
 
     public GameState GameState
     {
@@ -58,8 +79,11 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        responsiveFactor = RESPONSIVE_SIZE / ( RESPONSIVE_SCREEN.y /  RESPONSIVE_SCREEN.x);
         Application.targetFrameRate = targetFrameRate;
         Input.simulateMouseWithTouches = true;
+        CheckScreenRatio();
+        MakeCameraResponsive();
         //PrepareGame();
     }
 
@@ -77,5 +101,48 @@ public class GameManager : MonoBehaviour
     public void LoseGame()
     {
         _gameState = GameState.GameOver;
+    }
+
+    void CheckScreenRatio()
+    {
+        float aspect = (float)Screen.width / Screen.height;
+        if (aspect >= 0.72f)
+        {
+            ScreenRatio = ScreenRatio.VeryBroad;
+            return;
+        }
+        if (aspect >= 0.64f)
+        {
+            ScreenRatio = ScreenRatio.Broad;
+            return;
+        }
+
+        if (aspect >= 0.53f)
+        {
+            ScreenRatio = ScreenRatio.Normal;
+            return;
+        }
+
+        if (aspect >= 0.48f)
+        {
+            ScreenRatio = ScreenRatio.Narrow;
+            return;
+        }
+
+        if (aspect >= 0.44f)
+        {
+            ScreenRatio = ScreenRatio.VeryNarrow;
+            return;
+        }
+        ScreenRatio = ScreenRatio.SuperNarrow;
+    }
+
+
+    //Responsive size for camera is 5 with aspect ratio 1x2 (800x400)
+    void MakeCameraResponsive()
+    {
+        
+        
+        gameplayCamera.orthographicSize = responsiveFactor * (1.0f * Screen.height /Screen.width);
     }
 }
